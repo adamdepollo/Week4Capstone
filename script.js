4c5c7c5708b3731478b0b61e13086957ddde1c74
adamdepollo <adamdepollo@gmail.com>
2019-11-11 23:10:07 -0500

    beautified code

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var menuPrices = [
   7.25
 ];
 
-for (i = 0; i < menuItems.length; i++ ) {
+for (i = 0; i < menuItems.length; i++) {
   document.getElementsByClassName('itemName')[i].textContent = menuItems[i];
   document.getElementsByClassName('price')[i].textContent = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -27,13 +27,9 @@ for (i = 0; i < menuItems.length; i++ ) {
 }
 
 function getSubtotal() {
-  var vals = document.getElementsByClassName('inpt');/*
-  var it1cont = document.getElementById("it1").value;
-  var it2cont = document.getElementById("it2").value;
-  var it3cont = document.getElementById("it3").value;
-  var subtotal = (it1cont * menuPrices[0]) + (it2cont * menuPrices[1]) + (it3cont * menuPrices[2]);*/
+  var vals = document.getElementsByClassName('inpt');
   var subtotal = 0;
-  for (i = 0; i < vals.length; i ++) {
+  for (i = 0; i < vals.length; i++) {
     subtotal += vals[i].value * menuPrices[i];
   }
   document.getElementById('subTot').innerHTML = new Intl.NumberFormat('en-US', {
